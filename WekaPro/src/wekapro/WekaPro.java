c45fae7e1cf2fe4ef88e57a21f40cd0f76c75053
Admin <tnmdat183@gmail.com>
2021-04-07 21:43:49 +0700

    bai14 - emsemble method: Bagging, Boosting, Voting, Blending/Stacking

--- WekaPro/src/wekapro/WekaPro.java
+++ WekaPro/src/wekapro/WekaPro.java
@@ -5,7 +5,11 @@
  */
 package wekapro;
 
+import wekapro.emsembles.MyBaggingModel;
 import weka.classifiers.trees.J48;
+import wekapro.emsembles.MyBlendingModel;
+import wekapro.emsembles.MyBoostingModel;
+import wekapro.emsembles.MyVotingModel;
 
 /**
  *
@@ -75,13 +79,13 @@ public class WekaPro {
 //                "C:\\Users\\Admin\\Desktop\\weka_files\\iris_predict.arff");
 //        System.out.println(model);
 
-        MySVMModel model = new MySVMModel("",
-            "-C 1.0 -L 0.001 -P 1.0E-12 -N 0 -V -1 -W 1 -K \"weka.classifiers.functions.supportVector.PolyKernel -E 1.0 -C 250007\" -calibrator \"weka.classifiers.functions.Logistic -R 1.0E-8 -M -1 -num-decimal-places 4\"", null);
-        model.buildSVM("C:\\Users\\Admin\\Desktop\\weka_files\\iris_train.arff");
-        model.evaluateSVM("C:\\Users\\Admin\\Desktop\\weka_files\\iris_test.arff");
-        model.predictClassLabel("C:\\Users\\Admin\\Desktop\\weka_files\\iris_unlabel.arff",
-                "C:\\Users\\Admin\\Desktop\\weka_files\\iris_predict.arff");
-        System.out.println(model);
+//        MySVMModel model = new MySVMModel("",
+//            "-C 1.0 -L 0.001 -P 1.0E-12 -N 0 -V -1 -W 1 -K \"weka.classifiers.functions.supportVector.PolyKernel -E 1.0 -C 250007\" -calibrator \"weka.classifiers.functions.Logistic -R 1.0E-8 -M -1 -num-decimal-places 4\"", null);
+//        model.buildSVM("C:\\Users\\Admin\\Desktop\\weka_files\\iris_train.arff");
+//        model.evaluateSVM("C:\\Users\\Admin\\Desktop\\weka_files\\iris_test.arff");
+//        model.predictClassLabel("C:\\Users\\Admin\\Desktop\\weka_files\\iris_unlabel.arff",
+//                "C:\\Users\\Admin\\Desktop\\weka_files\\iris_predict.arff");
+//        System.out.println(model);
         
 //        MyKNNModel model = new MyKNNModel("",
 //            "-K 5 -W 0 -A \"weka.core.neighboursearch.LinearNNSearch -A \\\"weka.core.EuclideanDistance -R first-last\\\"\"", null);
@@ -90,6 +94,38 @@ public class WekaPro {
 //        model.predictClassLabel("C:\\Users\\Admin\\Desktop\\weka_files\\iris_unlabel.arff",
 //                "C:\\Users\\Admin\\Desktop\\weka_files\\iris_predict.arff");
 //        System.out.println(model);
+        
+//        MyBaggingModel model = new MyBaggingModel("",
+//            "-K 5 -W 0 -A \"weka.core.neighboursearch.LinearNNSearch -A \\\"weka.core.EuclideanDistance -R first-last\\\"\"", null);
+//        model.buildBaggingModel("C:\\Users\\Admin\\Desktop\\weka_files\\iris_train.arff");
+//        model.evaluateBaggingModel("C:\\Users\\Admin\\Desktop\\weka_files\\iris_test.arff");
+//        model.predictClassLabel("C:\\Users\\Admin\\Desktop\\weka_files\\iris_unlabel.arff",
+//                "C:\\Users\\Admin\\Desktop\\weka_files\\iris_predict_bag.arff");
+//        System.out.println(model);
+        
+//        MyBoostingModel model = new MyBoostingModel("",
+//            "-K 5 -W 0 -A \"weka.core.neighboursearch.LinearNNSearch -A \\\"weka.core.EuclideanDistance -R first-last\\\"\"", null);
+//        model.buildBoostingModel("C:\\Users\\Admin\\Desktop\\weka_files\\iris_train.arff");
+//        model.evaluateBoostingModel("C:\\Users\\Admin\\Desktop\\weka_files\\iris_test.arff");
+//        model.predictClassLabel("C:\\Users\\Admin\\Desktop\\weka_files\\iris_unlabel.arff",
+//                "C:\\Users\\Admin\\Desktop\\weka_files\\iris_predict_boost.arff");
+//        System.out.println(model);
+        
+//        MyVotingModel model = new MyVotingModel("",
+//            "-K 5 -W 0 -A \"weka.core.neighboursearch.LinearNNSearch -A \\\"weka.core.EuclideanDistance -R first-last\\\"\"", null);
+//        model.buildVotingModel("C:\\Users\\Admin\\Desktop\\weka_files\\iris_train.arff");
+//        model.evaluateVotingModel("C:\\Users\\Admin\\Desktop\\weka_files\\iris_test.arff");
+//        model.predictClassLabel("C:\\Users\\Admin\\Desktop\\weka_files\\iris_unlabel.arff",
+//                "C:\\Users\\Admin\\Desktop\\weka_files\\iris_predict_vote.arff");
+//        System.out.println(model);
+        
+        MyBlendingModel model = new MyBlendingModel("",
+            "-K 5 -W 0 -A \"weka.core.neighboursearch.LinearNNSearch -A \\\"weka.core.EuclideanDistance -R first-last\\\"\"", null);
+        model.buildBlendingModel("C:\\Users\\Admin\\Desktop\\weka_files\\iris_train.arff");
+        model.evaluateBlendingModel("C:\\Users\\Admin\\Desktop\\weka_files\\iris_test.arff");
+        model.predictClassLabel("C:\\Users\\Admin\\Desktop\\weka_files\\iris_unlabel.arff",
+                "C:\\Users\\Admin\\Desktop\\weka_files\\iris_predict_blending.arff");
+        System.out.println(model);
     }
     
 }
